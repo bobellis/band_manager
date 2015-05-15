ac1cb411bfd8dcd19a88dc26a33fe1d711b9280b
Bob <bobellis.tx@gmail.com>
2015-05-15 12:35:51 -0700

    Add band.erb file and paths to save venues to a band and view them

--- app.rb
+++ app.rb
@@ -28,5 +28,19 @@ end
 
 get('/band/:id') do
   @band = Band.find(params.fetch('id').to_i())
+  @venues = @band.venues()
+  @all_venues = Venue.all() - @venues
+  erb(:band)
+end
+
+patch('/band/:id') do
+  @band = Band.find(params.fetch('id'))
+  @venue_ids = params.fetch('venue_ids')
+  @venue_ids.each do |venue_id|
+    @new_venue = Venue.find(venue_id)
+    @band.venues.push(@new_venue)
+  end
+  @venues = @band.venues()
+  @all_venues = Venue.all() - @venues
   erb(:band)
 end
