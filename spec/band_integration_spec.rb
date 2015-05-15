de2452688610bfae1592ea48c245eaa71e0a4b97
Bob <bobellis.tx@gmail.com>
2015-05-15 13:53:07 -0700

    Add new integration spec to check adding a venue to a band

--- spec/band_integration_spec.rb
+++ spec/band_integration_spec.rb
@@ -2,7 +2,7 @@ ENV["RACK_ENV"] = "test"
 
 require('capybara/rspec')
 require('./app')
-# require('spec_helper')
+
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
@@ -18,6 +18,24 @@ describe('adding a new Band', {:type => :feature}) do
     visit('/')
     fill_in('new_band', :with => 'Test Band')
     click_button('Save Band')
-    expect(page).to have_content('Test Band')
+    expect(page).to have_content('Test band')
   end
+end
+
+describe('adding a venue to a band', {:type => :feature}) do
+  it('adds a venue to a band') do
+    visit('/')
+    click_link('Add Venues')
+    fill_in('new_venue', :with => 'New Test Venue')
+    click_button('Save Venue')
+    click_link("Go home!")
+    fill_in('new_band', :with => 'New Test Band')
+    click_button('Save Band')
+    click_link("New test band")
+    page.check("New test venue")
+    click_button("Add venues")
+    expect(page).to have_content('New test venue')
+
+  end
+
 end
